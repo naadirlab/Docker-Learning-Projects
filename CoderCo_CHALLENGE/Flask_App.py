64fd617c9fc3c92c569af37eb6aa12b81fd4d682
naadirlab <sharifnaadir@gmail.com>
2025-12-14 21:38:17 +0100

    added code to the Flask App

---
+++ CoderCo_CHALLENGE/Flask_App.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import redis 
+
+app = Flask(__name__)
+
+# Connect to the Redis database
+redis_client = redis.Redis(
+        host='localhost',
+        port=6379,
+        decode_responses=True
+    ) 
+
+# First Route: Displays a welcome message
+@app.route('/')
+def welcome():
+    return "Welcome to my Flask App!"
+    
+# Second Route: Displays the Visit Count
+@app.route('/count')
+def count():
+    count = redis_client.incr('visitor_count')
+    return f'Overall Visitors: {count}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
